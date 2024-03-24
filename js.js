c9b51f2f6997ef03cd87845c1dd3242dd358090c
Monika <codingbymon@gmail.com>
2024-03-24 14:39:56 +0100

    complete third task

--- js.js
+++ js.js
@@ -37,7 +37,10 @@ console.log({allAdults});
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
 
-
+const findId = comments.find(comment => {
+    return comment.id === 823423
+})
+console.log({findId});
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
